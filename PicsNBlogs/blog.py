68c8f354662ae754c41fb3530b510372443f2686
dsowles <d_sowles@mail.fhsu.edu>
2025-10-26 22:28:20 -0500

    Added bootstrap to base template

--- PicsNBlogs/blog.py
+++ PicsNBlogs/blog.py
@@ -21,10 +21,7 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     
-    # return render_template('blog/index.html', posts=posts)
-
-    # DELETE THIS
-    return '<h1>Blog Index</h1>'
+    return render_template('blog/index.html', posts=posts)
 
 
 ############################
@@ -57,9 +54,83 @@ def create():
             db.commit()
             return redirect(url_for('blog.index'))
 
-    # return render_template('blog/create.html')
+    return render_template('blog/create.html')
 
-    # DELETE THIS
-    return '<h1>Create Blog</h1>'
+
+##################################
+
+
+##################################
+# Blog Update Page Handler.      #
+##################################
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+
+#################################
+
+
+#######################################
+# Blog Delete Page handler.           #
+#######################################
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
+
+######################################
+
+
+
+#################################
+# Helper Functions              #
+#################################
+
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
 
 ##################################
